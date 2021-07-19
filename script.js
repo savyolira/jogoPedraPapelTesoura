4832d86f5a323aa4ad8e1f44fa27fe4e6e2e7089
savyolira <savyolira@gmail.com>
2021-07-19 13:46:06 -0300

    atualizações do código

--- script.js
+++ script.js
@@ -2,74 +2,44 @@ const pedra     = document.getElementById('pedra');
 const papel     = document.getElementById('papel');
 const tesoura   = document.getElementById('tesoura');
 const resultado = document.getElementById('resultado');
-
+const pedra1   = 1;
+const papel2   = 2;
+const tesoura3 = 3;
 pedra.addEventListener("click", function(){
-    const pedra1   = 1;
-    const papel2   = 2;
-    const tesoura3 = 3;
-    let pc = Math.floor(Math.random() * (tesoura3 - pedra1)) + pedra1;
-    
+    let pc = Math.floor(Math.random() * (tesoura3)) + pedra1;
     if (pc === pedra1){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou pedra, empatou!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou pedra, empatou!';
     }
     if (pc === papel2){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou papel, você perdeu!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou papel, você perdeu!';
     }
     if (pc === tesoura3){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou tesoura, você ganhou!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou tesoura, você ganhou!';
     }
+    console.log(pc);
 })
-
 papel.addEventListener("click", function(){
-    const pedra1   = 1;
-    const papel2   = 2;
-    const tesoura3 = 3;
-    let pc = Math.floor(Math.random() * (tesoura3 - pedra1)) + pedra1;
-    
+    let pc = Math.floor(Math.random() * (tesoura3)) + pedra1;
     if (pc === pedra1){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou pedra, você ganhou!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou pedra, você ganhou!';
     }
     if (pc === papel2){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou papel, empatou!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou papel, empatou!';
     }
     if (pc === tesoura3){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou tesoura, você perdeu!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou tesoura, você perdeu!';
     } 
     
 })
-
 tesoura.addEventListener("click", function(){
-    const pedra1   = 1;
-    const papel2   = 2;
-    const tesoura3 = 3;
-    let pc = Math.floor(Math.random() * (tesoura3 - pedra1)) + pedra1;
-    
+    let pc = Math.floor(Math.random() * (tesoura3)) + pedra1;
     if (pc === pedra1){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou pedra, você perdeu!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou pedra, você perdeu!';
     }
     if (pc === papel2){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou papel, você ganhou!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou papel, você ganhou!';
     }
     if (pc === tesoura3){
-        const span = document.createElement('span');
-        span.innerText = ' O computador tirou tesoura, empatou!';
-        resultado.appendChild(span);
+        resultado.innerText = ' O computador tirou tesoura, empatou!';
     } 
-    
 })
